f564492453a75d753ce56e0e6e11f959aba932a4
Henrique Martins de Souza <hszm2094@outlook.com>
2020-02-04 11:06:02 -0300

    Adicionado novos plugins para funcionamento das transições de tela

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ function typeWriter(elemento){
   const textoArray = elemento.innerHTML.split('');
   elemento.innerHTML = '';
   textoArray.forEach((letra, i) => {
-    setTimeout(() => elemento.innerHTML += letra, 185 * i);
+    setTimeout(() => elemento.innerHTML += letra, 175 * i);
   })
 }
 
 const titulo = document.querySelector('.animated_tittle');
 
+
 typeWriter(titulo);
+
